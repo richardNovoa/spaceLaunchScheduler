5262ea6c20ae1ca1a4e6183481d2b1c68bd4d144
Richard Novoa <56084025+richardNovoa@users.noreply.github.com>
2022-12-14 11:54:22 -0500

    added launches components

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.png';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import Launches from './components/Launches';
+
+const client = new ApolloClient({
+	uri: 'http://localhost:5001/graphql',
+	cache: new InMemoryCache()
+});
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<ApolloProvider client={client}>
+			<div className='container'>
+				<img
+					src={logo}
+					alt='SpaceX'
+					style={{ width: 300, margin: 'auto', display: 'block' }}
+				/>
+				<Launches />
+			</div>
+		</ApolloProvider>
+	);
 }
 
 export default App;
---
+++ client/src/components/Launches.js
@@ -0,0 +1,45 @@
+import React, { Component, Fragment } from 'react';
+import { gql, useQuery } from '@apollo/client';
+import LaunchItem from './LaunchItem';
+
+const LAUNCHES_QUERY = gql`
+	query LaunchesQuery {
+		launches {
+			flight_number
+			mission_name
+			launch_date_local
+			launch_success
+		}
+	}
+`;
+
+function DisplayLaunches() {
+	const { loading, error, data } = useQuery(LAUNCHES_QUERY);
+
+	if (loading) return <h4>Loading...</h4>;
+	if (error) console.log(error);
+	console.log(data);
+
+	return (
+		<Fragment>
+			{data.launches.map((launch) => (
+				<LaunchItem key={launch.flight_number} launch={launch} />
+			))}
+		</Fragment>
+	);
+}
+
+export class Launches extends Component {
+	render() {
+		return (
+			<div>
+				<Fragment>
+					<h1 className='display-4 my-3'>Launches</h1>
+					<DisplayLaunches />
+				</Fragment>
+			</div>
+		);
+	}
+}
+
+export default Launches;
